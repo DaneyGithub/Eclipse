b5ca4d504e348b0fb366f3950348ea2713f3efe4
DaneyGithub <daneybm@yahoo.com>
2017-10-23 17:34:02 +0200

    Update MyClass.java

--- TestProject/src/myPaa/MyClass.java
+++ TestProject/src/myPaa/MyClass.java
@@ -5,7 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		//Comment and the next line added
+		System.out.println("This is from Github");
 		System.out.println("Test edited later");
 
 	}
